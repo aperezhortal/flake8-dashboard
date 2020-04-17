c7c4eec3a8109dc2d44d72619bab082525048ca1
Andres Perez Hortal <andresperezcba@gmail.com>
2020-04-17 17:44:27 -0400

    Test path handling

---
+++ flake8_dashboard/tests/test_path_handling.py
@@ -0,0 +1,15 @@
+from pathlib import PurePath, PurePosixPath, Path
+
+from flake8_dashboard.utils import relative_path
+
+
+def test_path_handling():
+    """Test that the relative paths are handled always as Linux paths."""
+
+    home = PurePath(str(Path.cwd()))
+    subdirs = ["test", "a", "b", "c"]
+    full_path = home.joinpath(PurePath(*subdirs))
+
+    rel_path = relative_path(full_path, home)
+
+    assert rel_path == str(PurePosixPath(*subdirs))
