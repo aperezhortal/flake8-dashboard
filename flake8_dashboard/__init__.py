019d5689237a5317b922f4bc1356d88d351e57f7
Andres Perez Hortal <andresperezcba@gmail.com>
2020-09-04 18:16:57 -0400

    Bump version: 0.1.3 → 0.1.4

--- flake8_dashboard/__init__.py
+++ flake8_dashboard/__init__.py
@@ -6,7 +6,7 @@ flake8-dashboard
 A flake8 plugin to generate a HTML dashboard with all the flake8 violations.
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from .plugin import DashboardReporter
 
